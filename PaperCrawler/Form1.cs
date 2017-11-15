206daa2eb7d2afaee88d15ecb27f7ba99520cb13
DESKTOP-5J7ES5Q\furaga <furaga.fukahori@gmail.com>
2017-11-15 20:53:41 +0900

    first commit

---
+++ PaperCrawler/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace PaperCrawler
+{
+    public partial class Form1 : Form
+    {
+        class Crawl
+        {
+            public string URL { get; set; }
+            public string HTMLPath { get; set; }
+        }
+
+        class Scrape
+        {
+            public string HTMLPath { get; set; }
+            public string JSPath { get; set; }
+            public string ConferenceName { get; set; }
+        }
+
+        List<Crawl> crawls = new List<Crawl>();
+        List<Scrape> scrapes = new List<Scrape>();
+
+        string HTMLDirectory= "./data/html";
+        string JSDirectory = "./data/js/";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int year = 2005; year <= 2017; year++)
+            {
+                crawls.Add(new Crawl()
+                {
+                    URL = $"http://kesen.realtimerendering.com/sig{year}.html",
+                    HTMLPath = System.IO.Path.Combine(HTMLDirectory, $"SIGGRAPH_{year}.html"),
+                });
+            }
+
+            for (int year = 2008; year <= 2017; year++)
+            {
+                crawls.Add(new Crawl()
+                {
+                    URL = $"http://kesen.realtimerendering.com/siga{year}Papers.html",
+                    HTMLPath = System.IO.Path.Combine(HTMLDirectory, $"SIGGRAPH_Asia_{year}.html"),
+                });
+            }
+
+            if (Directory.Exists(HTMLDirectory) == false)
+            {
+                Directory.CreateDirectory(HTMLDirectory);
+            }
+
+            foreach (var c in crawls)
+            {
+                var siggraph = new PaperCrawlerLib.SIGGRAPH();
+                string result = siggraph.Crawl(c.URL, c.HTMLPath);
+                richTextBox1.Text += result + "\n";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(JSDirectory) == false)
+            {
+                Directory.CreateDirectory(JSDirectory);
+            }
+
+            var htmls = System.IO.Directory.GetFiles(HTMLDirectory).Where(path => path.EndsWith("html")).ToList();
+            foreach (var path in htmls)
+            {
+                string filename = Path.GetFileNameWithoutExtension(path);
+                string conferenceName = filename.Replace('_', ' ');
+
+                var siggraph = new PaperCrawlerLib.SIGGRAPH();
+                var papers = siggraph.Scrape(path, conferenceName);
+                if (papers != null)
+                {
+                    var json = JsonConvert.SerializeObject(papers, Formatting.Indented);
+                    string jsPath = Path.Combine(JSDirectory, filename + ".js");
+                    System.IO.File.WriteAllText(jsPath, "export default " + json);
+                    richTextBox1.Text += $"scrape: {papers.Count} jsons -> {jsPath}\n";
+                }
+                else
+                {
+                    richTextBox1.Text += "scrape: failed. \n";
+                }
+            }
+
+        }
+
+    }
+}
---
+++ PaperCrawlerLib/SIGGRAPH.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Parser.Html;
+using AngleSharp.Dom;
+using AngleSharp.Dom.Html;
+using AngleSharp.Extensions;
+
+namespace PaperCrawlerLib
+{
+    public class SIGGRAPH
+    {
+        public class Paper
+        {
+            public string Conference { get; set; }
+            public string Title { get; set; }
+            public string[] Authors { get; set; }
+            public string OfficialSiteURL { get; set; }
+            public string[] OtherURLs { get; set; }
+        }
+
+
+        public string Crawl(string url, string savepath)
+        {
+            try
+            {
+                var wc = new System.Net.WebClient();
+                wc.DownloadFile(url, savepath);
+                wc.Dispose();
+                return $"crawled: {url} -> {savepath}";
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+
+        public List<Paper> Scrape(string htmlPath, string conferenceName)
+        {
+            try
+            {
+                var parser = new HtmlParser();
+                var document = parser.Parse(System.IO.File.ReadAllText(htmlPath));
+
+                var h2s = document.QuerySelectorAll("h2").ToList();
+                var dls = document.QuerySelectorAll("dl").ToList();
+                System.Diagnostics.Debug.Assert(h2s.Count == dls.Count);
+
+
+                List<Paper> paperList = new List<Paper>();
+
+                for (int i = 0; i < h2s.Count; i++)
+                {
+                    var h2 = h2s[i].Text();
+
+                    var dl = dls[i];
+                    var dts = dl.QuerySelectorAll("dt").Where(dt =>false == string.IsNullOrWhiteSpace(dt.InnerHtml)).ToList();
+                    var dds = dl.QuerySelectorAll("dd").Where(dt => false == string.IsNullOrWhiteSpace(dt.InnerHtml)).ToList();
+                    if (dts.Count != dds.Count)
+                    {
+                        Console.WriteLine("scaraped: failed in " + h2 + " of " + htmlPath);
+                        continue;
+                    }
+
+                    for (int k = 0; k < dts.Count; k++)
+                    {
+                        var dt = dts[k];
+                        var dd = dds[k];
+
+                        Paper paper = new Paper();
+
+                        var title = dt.QuerySelector("b");
+                        var links = dt.QuerySelectorAll("a");
+                        paper.Conference = conferenceName;
+                        paper.Title = title.Text();
+                        paper.OtherURLs = links.Select(lnk => lnk.GetAttribute("href")).ToArray();
+
+                        paper.OfficialSiteURL = links.Length >= 1 ? links[0].GetAttribute("href") : "";
+                        paper.Authors = dd.Text().Split(',').Select(s => s.Trim()).ToArray();
+
+                        paperList.Add(paper);
+                    }
+                }
+
+                return paperList;
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.ToString() + "\n" + ex.StackTrace);
+                return null;
+            }
+        }
+    }
+}
